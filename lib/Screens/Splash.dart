e179661219611bbe67ce94fd2ed536d831fb0ff3
Nachiketa Pandey <81499541+nacpa@users.noreply.github.com>
2022-12-06 23:32:29 +0530

    Update Splash.dart

--- lib/Screens/Splash.dart
+++ lib/Screens/Splash.dart
@@ -18,7 +18,7 @@ class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
   @override
   State<Splash> createState() => _SplashState();
-}
+} 
 
 class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
   var subscription;
